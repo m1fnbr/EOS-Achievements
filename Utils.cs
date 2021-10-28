ec3f74803c541ad6755c9f91b5acb49e3b38a242
M1 <25163821+m1fnbr@users.noreply.github.com>
2021-10-28 11:41:28 -0400

    Getting Prefered Browser instead of just using brave

--- Utils.cs
+++ Utils.cs
@@ -22,7 +22,7 @@ namespace Claimer
             Console.WriteLineFormatted(Format,Color.White, Formatting);
         }
 
-        public static void Brave(string URL)
+        /*public static void Brave(string URL)
         {
             Process process = new Process();
             ProcessStartInfo startInfo = new ProcessStartInfo();
@@ -31,6 +31,32 @@ namespace Claimer
             startInfo.Arguments = $"https://www.epicgames.com/id/api/redirect?clientId=ec684b8c687f479fadea3cb2ad83f5c6&responseType=code";
             process.StartInfo = startInfo;
             process.Start();
+        } */
+    public static void Browse(string url)
+    {
+        string browserName = "iexplore.exe";
+        using (RegistryKey UserChoiceAnswer = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\Shell\Associations\UrlAssociations\http\UserChoice"))
+        {
+            if (UserChoiceAnswer != null)
+            {
+                object progIdValue = UserChoiceAnswer.GetValue("Progid");
+                if (progIdValue != null)
+                {
+                    if(progIdValue.ToString().ToLower().Contains("chrome"))
+                        browserName = "chrome.exe";
+                    else if(progIdValue.ToString().ToLower().Contains("firefox"))
+                        browserName = "firefox.exe";
+                    else if (progIdValue.ToString().ToLower().Contains("safari"))
+                        browserName = "safari.exe";
+                    else if (progIdValue.ToString().ToLower().Contains("opera"))
+                        browserName = "opera.exe";
+                    else if (prodIDValue,ToString().ToLower().Containts("brave")
+                             browserName = "brave.exe";
+                }
+            }
+        }
+
+        Process.Start(new ProcessStartInfo(browserName, url));
         }
     }
 }
