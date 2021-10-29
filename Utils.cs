4e23e6e225c811a921d224050757e07462f09aeb
M1 <25163821+m1fnbr@users.noreply.github.com>
2021-10-29 00:27:54 -0400

    Clean

--- Utils.cs
+++ Utils.cs
@@ -20,33 +20,28 @@ namespace Claimer
                 new Formatter(Log,Color.Blue)
 
             };
-            Console.WriteLineFormatted(Format,Color.White, Formatting);
+            Console.WriteLineFormatted(Format, Color.White, Formatting);
         }
-    public static void Browse(string url)
-    {
-        string browserName = "iexplore.exe";
-        using (RegistryKey UserChoiceAnswer = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\Shell\Associations\UrlAssociations\http\UserChoice"))
+        public static void Browse(string url)
         {
+            string Browser = "iexplore.exe";
+            RegistryKey UserChoiceAnswer = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\Shell\Associations\UrlAssociations\http\UserChoice");
             if (UserChoiceAnswer != null)
             {
                 object progIdValue = UserChoiceAnswer.GetValue("Progid");
                 if (progIdValue != null)
                 {
-                    if(progIdValue.ToString().ToLower().Contains("chrome"))
-                        browserName = "chrome.exe";
-                    else if(progIdValue.ToString().ToLower().Contains("firefox"))
-                        browserName = "firefox.exe";
-                    else if (progIdValue.ToString().ToLower().Contains("safari"))
-                        browserName = "safari.exe";
+                    if (progIdValue.ToString().ToLower().Contains("chrome"))
+                        Browser = "chrome.exe";
+                    else if (progIdValue.ToString().ToLower().Contains("firefox"))
+                        Browser = "firefox.exe";
                     else if (progIdValue.ToString().ToLower().Contains("opera"))
-                        browserName = "opera.exe";
+                        Browser = "opera.exe";
                     else if (progIdValue.ToString().ToLower().Contains("brave"))
-                             browserName = "brave.exe";
+                        Browser = "brave.exe";
                 }
+                Process.Start(new ProcessStartInfo(Browser, url));
             }
         }
-
-        Process.Start(new ProcessStartInfo(browserName, url));
         }
     }
-}
